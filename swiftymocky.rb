b6d384d3b2c66ae6b1097ea55a9cdac7cd78517c
Aurimas Žibas <aurimas.zibas@transfergo.com>
2024-05-15 16:21:33 +0300

    chore: Update SwiftyMocky version to 4.2.0

--- swiftymocky.rb
+++ swiftymocky.rb
@@ -1,8 +1,8 @@
 class Swiftymocky < Formula
   desc "Framework for automatic mock generation. Adds a set of handy methods, simplifying testing. One of the best and most complete solutions, including generics support and much more."
   homepage "https://github.com/MakeAWishFoundation/SwiftyMocky"
-  url "https://github.com/MakeAWishFoundation/SwiftyMocky/archive/refs/tags/4.1.0.tar.gz"
-  sha256 "729a95f6ed232ca1f660b6e549649dd1e6b9b4960583434e5de64232b1e1a3e3"
+  url "https://github.com/MakeAWishFoundation/SwiftyMocky/archive/refs/tags/4.2.0.tar.gz"
+  sha256 "0b517fc5e92dc7a3508e16f8d4f5a63b4ca7714ca7d955dfee87eea50917c82c"
   license "MIT"
 
   depends_on xcode: "13.0"
